df9f94a13f4b9856eca2e29b73aecdbedd46fd89
Chris <Christopher.glass@colorado.edu>
2015-09-04 15:39:33 -0600

    Update Glass_Assignment1.py
    fixed a few issues and added coments

--- Glass_Assignment1.py
+++ Glass_Assignment1.py
@@ -1,31 +1,35 @@
+#HW 1 CSCI 3202 intro to AI
+#Author: Chris Glass
+#done in python 3
+
 import queue
 import unittest
 TestIntegers=[1,2,3,5,8,10,20,30,1000,35];
-TreeInts={1,1,2,2,10,100,30,5,8,100,5}
+TreeInts=[0,1,2,1,3,2,10,20,5,8]
 
 class int_Queue(queue.Queue):
-	def __init__(self):
+	def __init__(self):			#extends pyhton queue
 		queue.Queue.__init__(self)
-	def put(self,item,*block,**timeout):
+	def put(self,item,*block,**timeout):	#checks for int type
 		if(isinstance(item,int)):
 			queue.Queue.put(self,item,block,timeout)
 		else:
 			return("item is not of type int")
 
-	def get(self,*block,**timeout):
+	def get(self,*block,**timeout):		#get just adds a print statement
 		g=queue.Queue.get(self,block,timeout)
 		print(g)
 		return g
 
 class stack():
-	def __init__(self):
+	def __init__(self):			
 		self.L=[]
-	def push(self,data):
+	def push(self,data):		#checks for integer type
 		if(isinstance(data,int)):
 			self.L.append(data)
-	def pop(self):
+	def pop(self):				
 		return self.L.pop()
-	def checkSize(self):
+	def checkSize(self):	
 		return len(self.L)
 
 class node():
@@ -41,45 +45,51 @@ class binary_tree():
 	def __init__(self):
 		self.root=node(0,None,None,None)
 
-	def _find(self,Value,N):
+	def _find(self,Value,N):		#helper function DFS search
 		if(N.key==Value):
 			return N
 		elif(N.left):
 			out=self._find(Value,N.left)
 			if(out!=0):
 				return out
-		elif(N.right):
+		if(N.right):
 			out=self._find(Value,N.right)
+			return out
 		else:
 			return 0;
 		return out;
 
-	def add(self,value,parentValue):
-		parent=self._find(parentValue,self.root);
-		if(parent):
-			N=node(value,None,None,parent)
-			if(parent.left):
-				if(parent.right):
-					print("Parent already has Two children");
-					return 0;
+	def add(self,value,parentValue):		
+		if(not parentValue):		#checks if parent is null create root node
+			self.root=node(value,None,None,None)
+		else:
+			parent=self._find(parentValue,self.root);	#checks if parent is in tree
+			if(parent):
+				N=node(value,None,None,parent)
+				if(parent.left):	
+					if(parent.right):
+						print("Parent already has Two children");
+						return 0;
+					else:
+						parent.right=N
+						return 1
 				else:
-					parent.right=N
+					parent.left=N
 					return 1
 			else:
-				parent.left=N
-				return 1
-
+				print("parent not found")	
 
 	def delete(self,value):
-		D=self._find(value,self.root)
+		D=self._find(value,self.root)	#searching for value
 		if(D):
-			if(D.left or D.right):
+			if(D.left or D.right):		#checking for leaf status
 				print("Node has children NOT DELETED")
-			else:
-				if(D.parent.left==D):
-					D.parent.left=none
+			else:						#deleteing leaf
+				if(D.parent.left==D):	
+					D.parent.left=None
 				else:
-					D.parent.rght=none
+					D.parent.rght=None
+				print("deleted")
 		else:
 			print("Value not in tree")
 
@@ -125,51 +135,66 @@ class graph:
 
 class QueueTest(unittest.TestCase):
 	def test_int(self):
+		print("testing integer type")
 		self.Q=int_Queue()
 		self.assertEqual(self.Q.put("test"),"item is not of type int")
+		print("finished")
+
 	def test_FuncTest(self):
+		print("testing queue functionality...")
 		self.Q=int_Queue()
 		for x in TestIntegers:
 			self.Q.put(x)
 		for x in TestIntegers:
 			self.assertEqual(self.Q.get(),x);
+		print("finished")
 
 class StackTest(unittest.TestCase):
 	def setUp(self):
 		self.S=stack()
 
 	def test_Add(self):
+		print("testing stack add()...")
 		for x in TestIntegers:
 			self.S.push(x)
 		self.assertEqual(self.S.checkSize(),10)
+		print("finished")
+
 
 	def test_Pop(self):
+		print("print testing stack pop()")
 		if(not self.S.checkSize()):
 			for x in TestIntegers:
 				self.S.push(x)
 
 		for x in reversed(TestIntegers):
 			self.assertEqual(self.S.pop(),x)
+		print("finished")
 
 
 class TreeTest(unittest.TestCase):
 	def setUp(self):
 		self.B=binary_tree()
-		self.B.add(1,0)
 
 	def test_Add(self):
+		print("testing add()...")
 		for x,y in zip(TestIntegers, TreeInts):
 			self.B.add(x,y)
-		#self.B.Print();
+		self.B.Print();
 
 	def test_Delete(self):
+		for x,y in zip(TestIntegers, TreeInts):
+			self.B.add(x,y)
+		print("testing delete()...")
 		self.B.delete(2)
 		self.B.delete(30)
+		self.B.delete(35)
 		self.B.Print()
-
+		print("finshed")
 
 class GraphTest(unittest.TestCase):
 	def setUp(self):
+		print("testing Graph...")
 		self.G=graph()
 		self.G.addVertex(8)
 		self.G.addVertex(1)
@@ -183,6 +208,7 @@ class GraphTest(unittest.TestCase):
 		self.G.addVertex(11)
 
 	def test_Edge(self):
+		print("testing add Edge...")
 		self.assertTrue(self.G.addEdge(1,2))
 		self.assertTrue(self.G.addEdge(1,3))
 		self.assertTrue(self.G.addEdge(1,4))
@@ -206,11 +232,13 @@ class GraphTest(unittest.TestCase):
 		self.assertTrue(self.G.addEdge(4,5))
 
 	def test_Find(self):
+		print("testing find...")
 		self.assertTrue(self.G.findVertex(1))
 		self.assertTrue(self.G.findVertex(5))
 		self.assertTrue(self.G.findVertex(10))
 		self.assertTrue(self.G.findVertex(2))
 		self.assertTrue(self.G.findVertex(4))
+		print("finished")
 
 if __name__=='__main__':
 	unittest.main()
